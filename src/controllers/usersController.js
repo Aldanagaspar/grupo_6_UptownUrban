d86a064d429b77f6e56b2b776a9daa5152961293
Ignacio Torres <ignacio25.it@gmail.com>
2023-08-22 18:54:32 -0300

    Rutas creadas para trabajar

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -4,6 +4,20 @@ const usersController = {
     },
     register: (req,res) => {
         res.render("./users/register");
+    },
+
+    listadoProductos: (req, res) => {
+        //obtener listado de productos
+        res.render('./users/myProducts');
+    },
+
+    crearProducto: (req, res) => {
+        res.render('./users/createProduct');
+    },
+
+    editarProducto: (req, res) => {
+        let idProducto = req.query.idProd;
+        res.render('./users/createProduct', idProducto);
     }
 }
 
--- src/routes/users.js
+++ src/routes/users.js
@@ -5,5 +5,8 @@ const usersController = require('../controllers/usersController');
 
 router.get('/login', usersController.login);
 router.get('/registro', usersController.register);
+router.get('/myProducts',usersController.listadoProductos);
+router.get('/myProducts/createProduct', usersController.crearProducto);
+router.get('/myProducts/createProduct/:idProducto', usersController.editarProducto);
 
 module.exports = router;
