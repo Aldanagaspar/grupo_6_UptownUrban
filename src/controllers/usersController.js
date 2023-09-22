96ef740bc2bd778baac2724881f4f66b1386069a
AlvaroR15 <alvaroramos048@gmail.com>
2023-09-21 22:13:09 -0300

    Form registro funcionando correctamente

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,3 +1,10 @@
+const bcrypt = require('bcryptjs');
+const fs = require('fs');
+const path = require('path');
+
+const usersFilePath = path.resolve(__dirname, '../data/users.json');
+
+
 const usersController = {
     login: (req,res) => {
         res.render("./users/login", {
@@ -12,7 +19,16 @@ const usersController = {
         });
     },
     addUser: (req, res) => {
-        
+        const users = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'))
+        const newUser = {
+            idUsuario : users[users.length - 1] + 1,
+            nombreCompleto: req.body.fullname,
+            email: req.body.email,
+            password: bcrypt.hashSync(req.body.password, 10),
+        }
+        users.push(newUser);
+        fs.writeFileSync(usersFilePath, JSON.stringify(users))
+        res.render('./users/profile', {users:users})
     }
 }
 
