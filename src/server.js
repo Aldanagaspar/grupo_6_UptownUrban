c856f91855bea1004c12c3525b45a7507a64b012
Man29toconas <toconascruz@gmail.com>
2023-12-10 20:09:23 -0300

    configuracion de cors

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require('cors');
 const path = require("path");
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
@@ -33,7 +34,14 @@ app.use(adminLoggedMiddleware);
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname,"/views"));
-
+app.use(cors());
+app.use(
+    cors({
+        origin:["http://localhost:8000", "http://localhost:3000"],
+        methods:"GET,POST,PUT,DELETE",
+        credentials:true
+    })
+);
 app.listen(PORT, () => {
     console.log("\n¡Servidor en línea! :D");
     console.log(`Iniciado en el puerto ${PORT}`);
